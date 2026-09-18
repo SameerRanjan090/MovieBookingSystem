fbbb7e292d1b7deeaa2e74933c65020670948b05
Sameer Ranjan <sameerranjan090@gmail.com>
2026-09-18 18:48:01 +0530

    Create Customer class for ticket booking system
    Added a Customer class to store customer information including ID, name, email, and phone.

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,1 +1,60 @@
+package model;
+
+/**
+ * Stores the basic information of a customer who books tickets.
+ */
+public class Customer {
+
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Customer(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String email, String phone) {
+        this(name, email, phone);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer ID: " + id
+                + " | Name: " + name
+                + " | Email: " + email
+                + " | Phone: " + phone;
+    }
+}
 
